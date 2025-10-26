12b73a320fe33863ea4aa4d7abb5afa40c43724e
Abreuzzs <tauanabrelcelularnovo@gmail.com>
2025-10-26 13:38:57 +0000

    Gpu underclock 1GHz to 900MHz

--- drivers/misc/mediatek/base/power/mt6768/mtk_gpufreq_core.h
+++ drivers/misc/mediatek/base/power/mt6768/mtk_gpufreq_core.h
@@ -9,8 +9,8 @@
 /**************************************************
  * MT6768/MT6767 : GPU DVFS OPP table Setting
  **************************************************/
-#define SEG_GPU_DVFS_FREQ0			(1000000)	/* KHz */
-#define SEG_GPU_DVFS_FREQ1			(975000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ0		        (900000)	        /* KHz */
+#define SEG_GPU_DVFS_FREQ1		        (897000)	        /* KHz */
 #define SEG_GPU_DVFS_FREQ2			(950000)	/* KHz */
 #define SEG_GPU_DVFS_FREQ3			(925000)	/* KHz */
 #define SEG_GPU_DVFS_FREQ4			(900000)	/* KHz */
@@ -42,8 +42,8 @@
 #define SEG_GPU_DVFS_FREQ30			(315000)	/* KHz */
 #define SEG_GPU_DVFS_FREQ31			(299000)	/* KHz */
 
-#define SEG_GPU_DVFS_VOLT0		(90000)		/* mV x 100 */
-#define SEG_GPU_DVFS_VOLT1		(87500)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT0		(87500)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT1		(86875)		/* mV x 100 */
 #define SEG_GPU_DVFS_VOLT2		(85000)		/* mV x 100 */
 #define SEG_GPU_DVFS_VOLT3		(82500)		/* mV x 100 */
 #define SEG_GPU_DVFS_VOLT4		(80000)		/* mV x 100 */
@@ -75,8 +75,8 @@
 #define SEG_GPU_DVFS_VOLT30		(56875)		/* mV x 100 */
 #define SEG_GPU_DVFS_VOLT31		(56250)		/* mV x 100 */
 
-#define SEG_GPU_DVFS_VSRAM0		(100000)	/* mV x 100 */
-#define SEG_GPU_DVFS_VSRAM1		(97500)	/* mV x 100 */
+#define SEG_GPU_DVFS_VSRAM0		(97500)	/* mV x 100 */
+#define SEG_GPU_DVFS_VSRAM1		(96875)	/* mV x 100 */
 #define SEG_GPU_DVFS_VSRAM2		(95000)	/* mV x 100 */
 #define SEG_GPU_DVFS_VSRAM3		(92500)		/* mV x 100 */
 #define SEG_GPU_DVFS_VSRAM4		(90000)		/* mV x 100 */
