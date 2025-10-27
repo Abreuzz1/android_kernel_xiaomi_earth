5cfa06827334c57958a83036fc5fb3c3052df1f1
Abreuzzs <tauanabrelcelularnovo@gmail.com>
2025-10-27 14:41:33 +0000

    Adapt exec.c para remover PF_PERF_CRITICAL e cpu_perf_mask

--- fs/exec.c
+++ fs/exec.c
@@ -1864,8 +1864,6 @@ static int __do_execve_file(int fd, struct filename *filename,
 		 if (unlikely(!strncmp(filename->name,
  					   HWCOMPOSER_BIN_PREFIX,
  					   strlen(HWCOMPOSER_BIN_PREFIX)))) {
- 			current->flags |= PF_PERF_CRITICAL;
- 			set_cpus_allowed_ptr(current, cpu_perf_mask);
  		}
 
 	/* execve succeeded */
